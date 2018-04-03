5cf9e0e6eb59e5c6779f84c0c71dd2bc7475d3de
Maheshwar Ajja <majja@codeaurora.org>
2018-04-02 17:30:29 -0700

    mm-video-v4l2: vdec: Fix extradata fd initialization
    memset driver context before initializing its elements
    else the extradata fd is wrongly re-initializing to zero
    and video playback usecase is trying to close fd zero which
    was not allocated by it causing video failures. Resolve
    the issue by doing proper initization.
    
    Change-Id: I30d587ae11d28c160f76df855a3c7a6a2b5e984a

--- mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
+++ mm-video-v4l2/vidc/vdec/src/omx_vdec_v4l2.cpp
@@ -738,6 +738,7 @@ omx_vdec::omx_vdec(): m_error_propogated(false),
     m_buffer_error(false)
 {
     m_poll_efd = -1;
+    memset(&drv_ctx, 0, sizeof(drv_ctx));
     drv_ctx.video_driver_fd = -1;
     drv_ctx.extradata_info.ion.data_fd = -1;
     drv_ctx.extradata_info.ion.dev_fd = -1;
@@ -822,7 +823,6 @@ omx_vdec::omx_vdec(): m_error_propogated(false),
 #endif
     memset(&m_cmp,0,sizeof(m_cmp));
     memset(&m_cb,0,sizeof(m_cb));
-    memset (&drv_ctx,0,sizeof(drv_ctx));
     memset (&h264_scratch,0,sizeof (OMX_BUFFERHEADERTYPE));
     memset (m_hwdevice_name,0,sizeof(m_hwdevice_name));
     memset(m_demux_offsets, 0, ( sizeof(OMX_U32) * 8192) );
@@ -841,7 +841,6 @@ omx_vdec::omx_vdec(): m_error_propogated(false),
 #ifdef _ANDROID_ICS_
     memset(&native_buffer, 0 ,(sizeof(struct nativebuffer) * MAX_NUM_INPUT_OUTPUT_BUFFERS));
 #endif
-    memset(&drv_ctx.extradata_info, 0, sizeof(drv_ctx.extradata_info));
 
     /* invalidate m_frame_pack_arrangement */
     memset(&m_frame_pack_arrangement, 0, sizeof(OMX_QCOM_FRAME_PACK_ARRANGEMENT));
